3b7973afae4d852662a61b58833301d5701fdcfa
Gugutataa <111111608+Gugutataa@users.noreply.github.com>
2022-08-18 19:37:42 +0200

    jett

---
+++ jett.py
@@ -0,0 +1,87 @@
+import time
+print("Best jett entry smokes")
+map = input("Enter map name:")
+map1 = map.lower()
+if "ascent" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nSmoke left from gen, nice to combine with a flash on left\nSmoke on top of generator, can peek haven, tetris or hell")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nSmoke shed, throw another smoke and close the door\nSmoke stairs, can continue with another smoke or go onto site")
+        time.sleep(420000)
+if "bind" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nOn pistol, buy a shorty, smoke and dash in lamps\nOn gun, dash onto pillar, take a fight or continue into lamps, you can also jump onto truck\nThis is very risky but you can also smoke and dash from the box onto truck")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nGo b long and dash with smoke on top of pallet\nGo hookah and smoke, then dash onto top site(risky)")
+        time.sleep(420000)
+if "breeze" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nSmoke right titty(default site for noobs), dash in and take fights\nSmoke between titties(pyramids for noobs), continue to backsite\nA riskier one is to updraft to bricks without smoke\nYou can also smoke halls and open doors to push onto site, if you have a sage ask her to wall doorway")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nSmoke and dash in front of pillar but use teammate util to clear left cubby")
+        time.sleep(420000)
+if "fracture" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nFrom main dash updraft onto top site\nSmoke and updraft in nest(if noone is there)\nSmoke box in main site, then dash in\nFrom dish updraft dash onto top site")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nSmoke and dash onto stairs\nUpdraft dash smoke onto tower from arcade")
+        time.sleep(420000)
+if "haven" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nAlways try to take verticality\nSmoke front of main container and dash in")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nSmoke the left box and updraft dash onto it\n")
+        time.sleep(420000)
+    if "c" in site1:
+        print("Smoke the top of the container and fight people backsite\nSmoke in front to the left side of the container and fight close angles")
+        time.sleep(420000)
+if "icebox" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nSmoke top nest, updraft and dash into it, first peek the site facing to mid, then smoke screens and peek right side\nSmoke on top of generator, updraft dash onto it, peek rafters and backsite")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nUpdraft on top of the white container, clear spots and glide through onto yellow or on site")
+        time.sleep(420000)
+if "split" in map1:
+    site = input("Enter site:")
+    site1 = site.lower()
+    if "a" in site1:
+        print("\n\n\nRIP OG SPLIT\nSmoke, updraft and dash the window on heaven\nSmoke and dash in front of flower pot")
+        time.sleep(420000)
+    if "b" in site1:
+        print("\n\n\nRIP OG SPLIT\nSmoke tle box on the left side, updraft and dash into it\nSmoke newbox and dash into it")
+if "pearl" in map1:
+    amogus = 0
+    print("")
+    print("")
+    print("")
+    while amogus < 3000:
+        print("not added bc this map is horseshit.")
+        print("L MAP")
+        amogus = amogus + 1
+    print("(pear)L")
+    print("(pear)L")
+    print("(pear)L")
+    print("(pear)L")
+    print("(pear)L")
+    print("(pear)L")
+    print("(pear)L")
+    print("(pear)L")
+    time.sleep(420000)
